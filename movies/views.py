87f7a564837e5b1db49817ba86b947f0144cf612
MistaMarcus <marcus.thompson5@yahoo.com>
2020-02-03 22:14:08 +0000

    Created django movie web app

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from airtable import Airtable
 import os
@@ -14,3 +14,45 @@ def home_page(request):
     search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER({Name}))")
     stuff_for_frontend = {'search_result': search_result}
     return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
+
+
+def create(request):
+    if request.method == 'POST':
+        data={'Name': request.POST.get('name'),
+             'Pictures': [{'url': request.POST.get('url') or "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png"}],
+              'Rating': int(request.POST.get('rating')),
+              'Notes': request.POST.get('notes')
+        }
+        try:
+            response = AT.insert(data)
+            messages.success(request,"New Movie Added: {}".format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request,"Got an error when trying to create a new movie: {}".format(e))
+    return redirect('/')
+
+def edit(request, movie_id):
+    if request.method =='POST':
+        data={
+            'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url') or "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png"}],
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes')
+        }
+
+        movieinfo = AT.update(movie_id, data)
+        messages.success(request,"Updated Movie: {}".format(movieinfo['fields'].get('Name')))
+
+    return redirect('/')
+
+def delete(request, movie_id):
+    try:
+        movie_name=AT.get(movie_id)['fields'].get('Name')
+        movie_delete = AT.delete(movie_id)
+    except Exception as e:
+        messages.warning(request, "Got an error when trying to delete a movie:{}".format(e))
+
+    # notify on delete
+    messages.warning(request,"Movie Deleted: {}".format(movie_name))
+
+    return redirect('/')
+
